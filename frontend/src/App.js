d679e8a62c163ecb4711a3b900609217c018cda1
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-12 23:20:30 -0500

    Added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoPage from "./Pages/TodoPage";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/TodoPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Components/Card/Card";
+
+const TodoPage = () => {
+  return (
+    <>
+      <Card />
+    </>
+  );
+};
+
+export default TodoPage;
