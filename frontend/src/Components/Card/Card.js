3d58423ee0c0ab4c80792aef2e75c592d1ee5c83
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-13 15:04:19 -0500

    Added tailwind css

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useTransition, animated } from "react-spring";
+// import { useTransition, animated } from "react-spring";
 
 const Card = ({ listOfTodos }) => {
   return (
