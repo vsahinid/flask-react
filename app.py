28195aa09361d04860ed930e815342e7a39d935d
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-12 20:26:21 -0500

    Added hello.html

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello world</h1>"
+    return render_template('hello.html', list_of_names=['Chris', 'Pizza', 'Ben'])
+
+
+@app.route('/<string:name>')
+def greet(name):
+    return "hello " + name
 
 
 if __name__ == '__main__':
