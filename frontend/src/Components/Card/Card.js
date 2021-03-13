2add845c8c396cd95287dc64915881475baebeb8
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-13 14:21:23 -0500

    Added hyperlink

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ listOfTodos }) => {
   return (
@@ -6,7 +7,9 @@ const Card = ({ listOfTodos }) => {
       {listOfTodos.map((todo) => {
         return (
           <ul key={todo.id}>
-            <li>{todo.content}</li>
+            <li>
+              <Link to={`${todo.id}`}>{todo.content}</Link>
+            </li>
           </ul>
         );
       })}
